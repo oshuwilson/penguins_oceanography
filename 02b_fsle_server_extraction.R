0f9429e5746f3f21bb0140fb3e4336322ed612f1
oshuwilson <joshc.wilson@hotmail.co.uk>
2025-01-06 14:38:09 +0000

    updated FSLE extraction

--- 02b_fsle_server_extraction.R
+++ 02b_fsle_server_extraction.R
@@ -14,25 +14,29 @@ files <- list.files(path = this.species, pattern = "*.RDS", full.names = T)
 
 #for each file
 for(file in files){
-
-#read in tracks
-tracks <- readRDS(file)
-
-#get list of x, y and time information from tracks
-a <- data.frame(x = tracks$x, y = tracks$y, time = tracks$date)
-
-#change negative lon values to 0-360 lon
-a <- a %>%
-  mutate(x = ifelse(x<0, 360 + x, x))
-
-#extract fsle
-fsle <- raadtools::extract(readfsle, a)
-tracks$fsle <- fsle
-
-#export
-saveRDS(tracks, paste0("fsle/", this.species, "/", this.site, "_", this.stage, "_fsle_extracted.RDS"))
-
-#print to signify completion
-print(file)
-
+  
+  #read in tracks
+  tracks <- readRDS(file)
+  
+  #get list of x, y and time information from tracks
+  a <- data.frame(x = tracks$x, y = tracks$y, time = tracks$date)
+  
+  #change negative lon values to 0-360 lon
+  a <- a %>%
+    mutate(x = ifelse(x<0, 360 + x, x))
+  
+  #extract fsle
+  fsle <- raadtools::extract(readfsle, a)
+  tracks$fsle <- fsle
+  
+  #list name of file without extension and folder
+  clean <- basename(file)
+  clean <- tools::file_path_sans_ext(clean)
+  
+  #export
+  saveRDS(tracks, paste0("fsle/", this.species, "/", clean, "_with_fsle.RDS"))
+  
+  #print to signify completion
+  print(clean)
+  
 }
